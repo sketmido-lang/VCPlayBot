79471090c751b948ea3643d8de85c0d366772ae9
sketmido-lang <sketmido@gmail.com>
2026-02-18 21:49:07 +0200

    Implement missing voice chat filters in filters.py
    Added missing filters for voice chat and updated comments.

--- VCPlayBot/helpers/filters.py
+++ VCPlayBot/helpers/filters.py
@@ -4,9 +4,16 @@ from typing import Union
 from pyrogram import filters
 
 # --- حركة الإنقاذ لعام 2026 ---
-# بنعرف 'edited' يدوي عشان أي ملف يطلبها يلاقيها وميعملش كراش
+# بنعرف الفلاتر الناقصة يدوي عشان أي ملف يطلبها يلاقيها وميعملش كراش
 if not hasattr(filters, "edited"):
     filters.edited = filters.create(lambda _, __, ___: False)
+
+# إصلاح فلاتر المكالمات المرئية/الصوتية الجديدة
+if not hasattr(filters, "voice_chat_ended"):
+    filters.voice_chat_ended = getattr(filters, "video_chat_ended", filters.create(lambda _, __, ___: False))
+
+if not hasattr(filters, "voice_chat_started"):
+    filters.voice_chat_started = getattr(filters, "video_chat_started", filters.create(lambda _, __, ___: False))
 
 from VCPlayBot.config import COMMAND_PREFIXES
 
