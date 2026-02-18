fd49b31e9c8652f13e94e87a72a98f258dc65090
sketmido-lang <sketmido@gmail.com>
2026-02-18 21:29:52 +0200

    Implement custom edited filter in filters.py
    Add custom 'edited' filter to prevent crashes with missing attribute.

--- VCPlayBot/helpers/filters.py
+++ VCPlayBot/helpers/filters.py
@@ -3,6 +3,11 @@ from typing import Union
 
 from pyrogram import filters
 
+# --- حركة الإنقاذ لعام 2026 ---
+# بنعرف 'edited' يدوي عشان أي ملف يطلبها يلاقيها وميعملش كراش
+if not hasattr(filters, "edited"):
+    filters.edited = filters.create(lambda _, __, ___: False)
+
 from VCPlayBot.config import COMMAND_PREFIXES
 
 # تم حذف filters.edited للتوافق مع تحديثات pyrogram 2.0+
